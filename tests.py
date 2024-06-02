5720fdcb5f4eb55a1db05868a032cc2586a2d3f2
FFO0XX <144718323+FFO0XX@users.noreply.github.com>
2024-06-02 17:54:05 +0200

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,12 @@
+
+
+# cols = 10
+# rows =10
+# xD = [[' ' for _ in range(cols)] for _ in range(rows)]
+# print(xD)
+
+# print("I ".join(str(i) for i in range(cols)))
+x = "abRacadabra"
+y = x.upper
+print(x)
+print(y)
